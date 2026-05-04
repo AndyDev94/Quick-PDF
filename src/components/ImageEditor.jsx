cba8144465987a664d2f3c6f3d563780594c01f3
AndyDev94 <guptaneesh94@gmail.com>
2026-05-04 06:06:08 +0530

    Restore zoom slider visibility in crop mode (hides only on interaction)

--- src/components/ImageEditor.jsx
+++ src/components/ImageEditor.jsx
@@ -894,7 +894,7 @@ const ImageEditor = ({ photo, onSave, onCancel, onRemove }) => {
           </div>
     
           <AnimatePresence>
-            {!cropMode && !isCropping && !isInteracting && (
+            {!isCropping && !isInteracting && (
               <motion.div 
                 initial={{ opacity: 0, scale: 0.8, x: 20 }}
                 animate={{ opacity: 1, scale: 1, x: 0 }}
